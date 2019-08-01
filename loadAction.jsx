b240a58181b37dfd44833f20fe638585e9857cf3
tokyosheep <kawanoshuji@Pro.local>
2019-08-02 07:41:32 +0900

    firstcommit

---
+++ loadAction.jsx
@@ -0,0 +1,22 @@
+(function(){
+function fromHex(h) {
+    var s = ''
+    for (var i = 0; i < h.length; i+=2) {
+        s += String.fromCharCode(parseInt(h.substr(i, 2), 16))
+    }
+    return decodeURIComponent(escape(s))
+}
+
+function toHex(s) {
+    // utf8 to latin1
+    var s = unescape(encodeURIComponent(s))
+    var h = ''
+    for (var i = 0; i < s.length; i++) {
+        h += s.charCodeAt(i).toString(16)
+    }
+    return h
+}
+
+$.writeln(fromHex("e382a2e382afe382b7e383a7e383b32032"));
+$.writeln(fromHex("e382bbe38383e3838831"));
+})();
